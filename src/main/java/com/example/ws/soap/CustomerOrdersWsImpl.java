e9e7daef31a42ab1d05118def5dbf33435b4134d
moumitacloud08 <moumitacloud08@gmail.com>
2024-05-03 09:15:46 -0700

    Implement getorders

---
+++ src/main/java/com/example/ws/soap/CustomerOrdersWsImpl.java
@@ -0,0 +1,58 @@
+package com.example.ws.soap;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.bharath.ws.trainings.CreateOrdersRequest;
+import com.bharath.ws.trainings.CreateOrdersResponse;
+import com.bharath.ws.trainings.CustomerOrdersPortType;
+import com.bharath.ws.trainings.GetOrdersRequest;
+import com.bharath.ws.trainings.GetOrdersResponse;
+import com.bharath.ws.trainings.Order;
+import com.bharath.ws.trainings.Product;
+
+public class CustomerOrdersWsImpl implements CustomerOrdersPortType {
+	
+	Map<BigInteger,List<Order>> customerOrders = new HashMap<>();
+	int currentId;
+	
+	public CustomerOrdersWsImpl() {
+		init();
+	}
+	
+	public void init() {
+		List<Order> orders = new ArrayList<>();
+		Order order =new Order();
+		order.setId(BigInteger.valueOf(1));
+		
+		Product product = new Product();
+		product.setId("1");
+		product.setDescription("IPhone");
+		product.setQuantity(BigInteger.valueOf(3));
+		order.getProduct().add(product);
+		
+		orders.add(order);
+		customerOrders.put(BigInteger.valueOf(++currentId), orders);
+		
+	}
+
+	@Override
+	public GetOrdersResponse getOrders(GetOrdersRequest request) {
+		BigInteger customerId = request.getCustomerId();
+		List<Order> orders = customerOrders.get(customerId);
+		
+		GetOrdersResponse response = new GetOrdersResponse();
+		response.getOrder().addAll(orders);
+		return response;
+	}
+
+	@Override
+	public CreateOrdersResponse createOrders(CreateOrdersRequest parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
